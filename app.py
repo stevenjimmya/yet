b6295047d8daee9a5cd6392fc87ffe4f87ca1a6b
stevenjimmya <stevenjimmya@gmail.com>
2026-06-27 13:18:53 -0400

    add home page template

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 # we're using flask as the lirary
-
+# render_template allows to find an HTML file 
+# in the templates folder and send it to the browser
 app = Flask(__name__)
 # create an instance of the Flask class, which will be our app
 
 @app.route('/')
 def home():
-    return "Yet is running." 
+    return render_template('index.html') 
 # when someone visits the page, run the home function,
-# and send back the text "Yet is running."
+# and send back the HTML template.
 
 if __name__ == "__main__":
     app.run(debug=True)
