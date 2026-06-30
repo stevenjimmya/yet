569033d88aaeae206e1f2351714bfc77bbc7cf09
stevenjimmya <stevenjimmya@gmail.com>
2026-06-29 21:10:07 -0400

    save intention to json file

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
+
+import json
+from datetime import date
 from flask import Flask, render_template, request, jsonify
 # we're using flask as the lirary
 # render_template allows to find an HTML file 
 # in the templates folder and send it to the browser
 app = Flask(__name__)
 # create an instance of the Flask class, which will be our app
-
 @app.route('/')
 def home():
-    return render_template('index.html') 
-# when someone visits the page, run the home function,
-# and send back the HTML template.
-
+    return render_template('index.html')
 
 @app.route('/set_intention', methods=['POST'])
 def set_intention():
     data = request.get_json()
     intention = data.get('intention')
-    print("Intention set:", intention)
+    
+    entry = {
+        "intention": intention,
+        "date": str(date.today())
+    }
+    
+    with open("intentions.json", "w") as f:
+        json.dump(entry, f)
+    
+    print("Saved intention:", intention)
     return jsonify({"status": "success"})
 
 
